2edaab25c3effa81afb44712a45370a0142521d3
Anton <antonlevitan86@gmail.com>
2018-12-19 15:03:38 +0200

    playing around with PCA

--- PCA_RNA_Seq.py
+++ PCA_RNA_Seq.py
@@ -2,66 +2,63 @@ import pandas as pd
 import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
-
+from matplotlib.ticker import NullFormatter
+from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
+from scipy import stats
 
-data = pd.read_excel('output.xlsx', header=1)
-norm_data = data.loc[:'CR_10860C', 'A15_in_A:normalized.counts':'A3_out_C:normalized.counts'].dropna()
-# print(norm_data.tail())
-experiment_title = 'Alex Data'
-k = 10
+experiment_title = 'Alex Drug Screen'
 
-# generate example list genes
-# genes = ['gene' + str(i) for i in range(1, 101)]
+# data = pd.read_excel('alex_drug_screen.xlsx', index_col=0)
+# norm_data = data.loc[:'CR_10860C', 'A15_in_A:normalized.counts':'A3_out_C:normalized.counts'].dropna()
+# # norm_data = data
+#
+# k = 10
+#
+# # generate example list genes
+# # genes = ['gene' + str(i) for i in range(1, 101)]
+#
+# #  generate example treatments and samples
+# # wt = ['wt' + str(i) for i in range(1, 6)]
+# # ko = ['ko' + str(i) for i in range(1, 6)]
+#
+# # data = pd.DataFrame(columns=[*wt, *ko], index=genes)
+# # # print(data.head())
+#
+# # for gene in data.index:
+# #     data.loc[gene, 'wt1':'wt5'] = np.random.poisson(lam=rd.randrange(10, 1000), size=5)
+# #     data.loc[gene, 'ko1':'ko5'] = np.random.poisson(lam=rd.randrange(10, 1000), size=5)
+#
+# # similar to StandardScaler() + fit_transform
+# scaled_data = preprocessing.scale(norm_data.T)
+#
+# # calling PCA object, fitting and transforming the scaled data to pca
+# pca = PCA(n_components=2)
+# pca.fit(scaled_data)
+# pca_data = pca.transform(scaled_data)
+#
+# # % of variation of each pca
+# per_var = np.round(pca.explained_variance_ratio_ * 100, decimals=1)
+# labels = ['PC' + str(x) for x in range(1, len(per_var) + 1)]
+#
+# # PCA per sample
+# pca_per_sample = pd.DataFrame(pca_data, index=[norm_data.columns.values], columns=labels)
+#
+# # take a look into the loading scores
+# # loading scores of PC1
+# loading_scores = pd.Series(pca.components_[0], index=norm_data.index.values)
+# # sort by absolute value
+# sorted_loading_scores = loading_scores.abs().sort_values(ascending=False)
+# # take the top k genes; highest loading value=highest contribution to variance in a PC
+# top_k_genes = sorted_loading_scores[0:k].index.values
+# # print(top_k_genes)
 
+def scree_plot(variances, pc_labels, title=experiment_title):
 
-#  generate example treatments and samples
-# wt = ['wt' + str(i) for i in range(1, 6)]
-# ko = ['ko' + str(i) for i in range(1, 6)]
+    """ Charts the explained variance of each PC """
 
-
-# data = pd.DataFrame(columns=[*wt, *ko], index=genes)
-# # print(data.head())
-
-# for gene in data.index:
-#     data.loc[gene, 'wt1':'wt5'] = np.random.poisson(lam=rd.randrange(10, 1000), size=5)
-#     data.loc[gene, 'ko1':'ko5'] = np.random.poisson(lam=rd.randrange(10, 1000), size=5)
-
-# print(data.head())
-# print(data.shape)
-
-# similar to StandardScaler() + fit_transform
-scaled_data = preprocessing.scale(norm_data.T)
-
-# calling PCA object, fitting and transforming the scaled data to pca
-pca = PCA()
-pca.fit(scaled_data)
-pca_data = pca.transform(scaled_data)
-
-# % of variation of each pca
-per_var = np.round(pca.explained_variance_ratio_ * 100, decimals=1)
-labels = ['PC' + str(x) for x in range(1, len(per_var) + 1)]
-
-# PCA per sample
-pca_per_sample = pd.DataFrame(pca_data, index=[norm_data.columns.values], columns=labels)
-print(pca_per_sample)
-
-# take a look into the loading scores
-# loading scores of PC1
-loading_scores = pd.Series(pca.components_[0], index=norm_data.index.values)
-# sort by absolute value
-sorted_loading_scores = loading_scores.abs().sort_values(ascending=False)
-# take the top k genes; highest loading value=highest contribution to variance in a PC 
-top_k_genes = sorted_loading_scores[0:k].index.values
-# print(loading_scores[top_k_genes])
-
-
-def scree_plot(variances, labels, title=experiment_title):
-
-    ''' Charts the explained variance of each PC '''
-
-    plt.bar(x=range(1, len(variances)+1), height=variances, tick_label=labels)
+    plt.bar(x=range(1, len(variances)+1), height=variances, tick_label=pc_labels)
     plt.ylabel('Percentage of Explained Variance')
     plt.xlabel('Principal Component')
     plt.title(title + ' Scree Plot')
@@ -71,7 +68,7 @@ def scree_plot(variances, labels, title=experiment_title):
 
 def pca_plot(pca_df, title=experiment_title):
 
-    ''' Plots PC1 versus PC2 '''
+    """ Plots PC1 versus PC2 """
 
     plt.scatter(pca_df.PC1, pca_df.PC2)
 
@@ -86,5 +83,81 @@ def pca_plot(pca_df, title=experiment_title):
     plt.show()
 
 
-scree_plot(per_var, labels)
-pca_plot(pca_per_sample)
+def pca_scatter_all(df):
+
+    features = df.columns
+    # Separating out the features
+    x = df.loc[:, features].values
+    # Separating out the target
+    y = df.index.values
+    # Standardizing the features
+    x = StandardScaler().fit_transform(x)
+
+
+    # loading_values_plot(pca_per_sample)
+    # scree_plot(per_var, labels)
+    # pca_plot(pca_per_sample)
+
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(x)
+    principalDf = pd.DataFrame(data = principalComponents
+                 , columns = ['principal component 1', 'principal component 2'])
+
+    finalDf = pd.concat([principalDf, pd.Series(df.index.values)], axis = 1)
+
+    tested_feature_1 = 'principal component 1'
+    tested_feature_2 = 'principal component 2'
+    x = finalDf[tested_feature_1]
+    y = finalDf[tested_feature_2]
+
+    # definitions for the axes
+    left, width = 0.1, 0.65
+    bottom, height = 0.1, 0.65
+    bottom_h = left_h = left + width + 0.02
+
+    rect_scatter = [left, bottom, width, height]
+    rect_histx = [left, bottom_h, width, 0.2]
+    rect_histy = [left_h, bottom, 0.2, height]
+
+    # start with a rectangular Figure
+    plt.figure(1, figsize=(8, 8))
+    axScatter = plt.axes(rect_scatter)
+    plt.xlabel(tested_feature_1, fontsize=18)
+    plt.ylabel(tested_feature_2, fontsize=18)
+
+    axHistx = plt.axes(rect_histx)
+    axHisty = plt.axes(rect_histy)
+
+    # no labels
+    nullfmt = NullFormatter()
+    axHistx.xaxis.set_major_formatter(nullfmt)
+    axHisty.yaxis.set_major_formatter(nullfmt)
+
+    # the scatter plot:
+    axScatter.scatter(x, y, s=6)
+
+    xbins = np.linspace(min(x), max(x), 30)
+    ybins = np.linspace(min(y), max(y), 30)
+
+    axHistx.hist(x, bins=xbins)
+    axHisty.hist(y, bins=ybins, orientation='horizontal')
+
+    plt.savefig('both_' + tested_feature_1 + ' vs. ' + tested_feature_2 + '.png')
+    plt.show()
+
+
+
+df = pd.read_excel('alex_drug_screen.xlsx', index_col=0)
+df = df.dropna().drop(labels='Essintial', axis=1)
+df['zscore_(FLC-NoDrug)'] = stats.zscore(list(df['S score (FLC-NoDrug)']))
+df['zscore_(FLC+Geld-FLC)'] = stats.zscore(list(df['S score (FLC+Geld-FLC)']))
+df['pvalue (FLC-NoDrug)'] = stats.norm.sf(abs(df['zscore_(FLC-NoDrug)']))
+df['pvalue (FLC+Geld-FLC)'] = stats.norm.sf(abs(df['zscore_(FLC+Geld-FLC)']))
+df2 = df[(df['pvalue (FLC-NoDrug)'] < 0.05) | (df['pvalue (FLC+Geld-FLC)'] < 0.05)]
+df.to_csv('zscore_Alex.csv')
+df2.to_csv('zscore_Alex_filtered.csv')
+
+# pca_scatter_all(df2)
+
+plt.scatter(df2['S score (FLC-NoDrug)'], df2['S score (FLC+Geld-FLC)'])
+plt.show()
